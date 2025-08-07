398846dd83f91d4db918ca141adefbf37c4e5a29
Gautam Jain <rpgautam.jain@gmail.com>
2025-08-08 00:41:43 +0530

    changed copyright

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -263,7 +263,7 @@ export default function Index() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 कारोबार Connect. All rights reserved.</p>
+            <p>&copy; 2025 कारोबार Connect. All rights reserved.</p>
           </div>
         </div>
       </footer>
